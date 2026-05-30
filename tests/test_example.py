15518b0dabdd50d6d1c87623746ecc3a66ed2375
bmudaliyar <mudaliyar.b@northeastern.edu>
2026-05-30 09:37:42 +0530

    Minor Fix

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,5 +1,18 @@
+from playwright.sync_api import sync_playwright
 from playwright.sync_api import Page
 
-def test_homepage_has_playwright_title(page: Page) -> None:
-    page.goto('https://playwright.dev')
-    assert 'Playwright' in page.title()
+def test_google_search():
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=False)
+
+        page = browser.new_page()
+        page.goto("https://www.google.com")
+
+        page.fill('textarea[name="q"]', "Playwright Python")
+        page.press('textarea[name="q"]', "Enter")
+
+        page.wait_for_load_state("networkidle")
+
+        assert "Playwright" in page.title()
+
+        browser.close()
